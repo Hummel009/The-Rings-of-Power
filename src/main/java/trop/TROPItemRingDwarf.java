0d546fc5159621767ca961e2c7cd4bc860f16691
Hummel009 <emperorbur@gmail.com>
2022-07-28 12:27:31 +0300

    better

--- src/main/java/trop/TROPItemRingDwarf.java
+++ src/main/java/trop/TROPItemRingDwarf.java
@@ -1,6 +1,6 @@
 package trop;
 
-import net.minecraft.entity.*;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.*;
 import net.minecraft.potion.*;
@@ -16,9 +16,9 @@ public class TROPItemRingDwarf extends Item {
 
 	@Override
 	public void onUpdate(ItemStack itemstack, World world, Entity entity, int par4, boolean par5) {
-		if (entity instanceof EntityLivingBase) {
-			((EntityLivingBase) entity).addPotionEffect(new PotionEffect(Potion.nightVision.id, 220, 2));
-			((EntityLivingBase) entity).addPotionEffect(new PotionEffect(Potion.digSpeed.id, 20, 2));
+		if (entity instanceof EntityPlayer) {
+			((EntityPlayer) entity).addPotionEffect(new PotionEffect(Potion.nightVision.id, 220, 2));
+			((EntityPlayer) entity).addPotionEffect(new PotionEffect(Potion.digSpeed.id, 20, 2));
 		}
 	}
 }
