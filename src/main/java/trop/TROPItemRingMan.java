00aca6e850fd8d617989f84064ddfef9e9c59b4d
Hummel009 <emperorbur@gmail.com>
2022-08-02 15:28:45 +0300

    refactoring

--- src/main/java/trop/TROPItemRingMan.java
+++ src/main/java/trop/TROPItemRingMan.java
@@ -3,20 +3,11 @@ package trop;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
+import net.minecraft.potion.*;
+import net.minecraft.util.*;
 import net.minecraft.world.World;
 
 public class TROPItemRingMan extends TROPItemRingBase {
-
-	@Override
-	public ActionResult<ItemStack> use(World world, PlayerEntity entity, Hand hand) {
-		entity.addEffect(new EffectInstance(Effects.REGENERATION, 3600, 2));
-		entity.addEffect(new EffectInstance(Effects.HEALTH_BOOST, 3800, 2));
-		return super.use(world, entity, hand);
-	}
 
 	@Override
 	public void inventoryTick(ItemStack itemstack, World world, Entity entity, int par4, boolean par5) {
@@ -24,5 +15,12 @@ public class TROPItemRingMan extends TROPItemRingBase {
 			((PlayerEntity) entity).addEffect(new EffectInstance(Effects.DAMAGE_BOOST, 20, 2));
 			((PlayerEntity) entity).addEffect(new EffectInstance(Effects.NIGHT_VISION, 240, 2));
 		}
+	}
+
+	@Override
+	public ActionResult<ItemStack> use(World world, PlayerEntity entity, Hand hand) {
+		entity.addEffect(new EffectInstance(Effects.REGENERATION, 3600, 2));
+		entity.addEffect(new EffectInstance(Effects.HEALTH_BOOST, 3800, 2));
+		return super.use(world, entity, hand);
 	}
 }
