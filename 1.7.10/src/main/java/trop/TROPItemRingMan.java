3cac6adf7fa9d1c03614582c28583de243c194df
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-02-06 15:45:11 +0300

    nerf

--- 1.7.10/src/main/java/trop/TROPItemRingMan.java
+++ 1.7.10/src/main/java/trop/TROPItemRingMan.java
@@ -9,16 +9,17 @@ import net.minecraft.world.World;
 public class TROPItemRingMan extends Item {
 	@Override
 	public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer entity) {
-		entity.addPotionEffect(new PotionEffect(Potion.regeneration.id, 3600, 2));
-		entity.addPotionEffect(new PotionEffect(Potion.field_76434_w.id, 3800, 2));
+		entity.addPotionEffect(new PotionEffect(Potion.regeneration.getId(), 3600, 2));
+		entity.addPotionEffect(new PotionEffect(Potion.field_76434_w.getId(), 3800, 2));
 		return super.onItemRightClick(itemStack, world, entity);
 	}
 
 	@Override
 	public void onUpdate(ItemStack itemstack, World world, Entity entity, int par4, boolean par5) {
 		if (entity instanceof EntityPlayer) {
-			((EntityPlayer) entity).addPotionEffect(new PotionEffect(Potion.damageBoost.id, 20, 2));
-			((EntityPlayer) entity).addPotionEffect(new PotionEffect(Potion.nightVision.id, 220, 2));
+			((EntityPlayer) entity).addPotionEffect(new PotionEffect(Potion.damageBoost.getId(), 20, 1));
+			((EntityPlayer) entity).addPotionEffect(new PotionEffect(Potion.nightVision.getId(), 220));
+			((EntityPlayer) entity).addPotionEffect(new PotionEffect(Potion.resistance.getId(), 20, 1));
 		}
 	}
 }
--- 1.7.10/src/main/java/trop/TROPItemRingNaria.java
+++ 1.7.10/src/main/java/trop/TROPItemRingNaria.java
@@ -9,15 +9,15 @@ import net.minecraft.world.World;
 public class TROPItemRingNaria extends Item {
 	@Override
 	public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer entity) {
-		entity.addPotionEffect(new PotionEffect(Potion.regeneration.id, 3600, 2));
-		entity.addPotionEffect(new PotionEffect(Potion.field_76434_w.id, 3800, 2));
+		entity.addPotionEffect(new PotionEffect(Potion.regeneration.getId(), 3600, 2));
+		entity.addPotionEffect(new PotionEffect(Potion.field_76434_w.getId(), 3800, 2));
 		return super.onItemRightClick(itemStack, world, entity);
 	}
 
 	@Override
 	public void onUpdate(ItemStack itemstack, World world, Entity entity, int par4, boolean par5) {
 		if (entity instanceof EntityPlayer) {
-			((EntityPlayer) entity).addPotionEffect(new PotionEffect(Potion.fireResistance.id, 20, 2));
+			((EntityPlayer) entity).addPotionEffect(new PotionEffect(Potion.fireResistance.getId(), 20));
 		}
 	}
 }
