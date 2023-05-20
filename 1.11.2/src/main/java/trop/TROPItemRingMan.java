b47a32710f7e8111bcf9671dab14e817a264758c
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-05-20 12:42:35 +0300

    1.11 works

---
+++ 1.11.2/src/main/java/trop/TROPItemRingMan.java
@@ -0,0 +1,29 @@
+package trop;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+
+public class TROPItemRingMan extends Item {
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer entity, EnumHand hand) {
+		entity.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 3600, 2));
+		entity.addPotionEffect(new PotionEffect(MobEffects.HEALTH_BOOST, 3800, 2));
+		return super.onItemRightClick(world, entity, hand);
+	}
+
+	@Override
+	public void onUpdate(ItemStack itemstack, World world, Entity entity, int par4, boolean par5) {
+		if (entity instanceof EntityPlayer) {
+			((EntityPlayer) entity).addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 20, 1));
+			((EntityPlayer) entity).addPotionEffect(new PotionEffect(MobEffects.NIGHT_VISION, 220));
+			((EntityPlayer) entity).addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 20, 1));
+		}
+	}
+}
