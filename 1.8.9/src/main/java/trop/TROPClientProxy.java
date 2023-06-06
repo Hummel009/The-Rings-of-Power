ed22abee6db3a380f35fee4ed86074e024368515
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-06-06 11:12:03 +0300

    1.9.4

--- 1.8.9/src/main/java/trop/TROPClientProxy.java
+++ 1.8.9/src/main/java/trop/TROPClientProxy.java
@@ -3,10 +3,13 @@ package trop;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class TROPClientProxy extends TROPCommonProxy {
 
 	@Override
+	@SideOnly(Side.CLIENT)
 	public void registerRenders() {
 		for (Item item : TROP.CONTENT) {
 			Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation("trop:" + item.getUnlocalizedName().substring(5), "inventory"));
