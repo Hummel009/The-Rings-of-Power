8857f773d45353f0f7bb9b7b34c43bbfecf26385
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-06-09 23:02:30 +0300

    Протестировано

--- 1.4.7/src/main/java/trop/TROPItemRingBase.java
+++ 1.4.7/src/main/java/trop/TROPItemRingBase.java
@@ -6,6 +6,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
+import java.util.List;
+
 public class TROPItemRingBase extends Item {
 	public TROPItemRingBase(int id) {
 		super(id);
@@ -13,6 +15,13 @@ public class TROPItemRingBase extends Item {
 
 	public void a(ItemStack itemStack, World world, Entity entity, int i, boolean b) {
 		onUpdate(itemStack, world, entity, i, b);
+	}
+
+	public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer, List list, boolean b) {
+	}
+
+	public void a(ItemStack itemStack, EntityPlayer entityPlayer, List list, boolean b) {
+		addInformation(itemStack, entityPlayer, list, b);
 	}
 
 	public ItemStack a(ItemStack itemStack, World world, EntityPlayer entityPlayer) {
