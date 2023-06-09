f395c1d730f25a7aafec138bd99e2eb8997b31c6
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-06-09 22:54:47 +0300

    Протестировано 1.3.2

--- 1.3.2/src/main/java/trop/TROPItemRingBase.java
+++ 1.3.2/src/main/java/trop/TROPItemRingBase.java
@@ -2,6 +2,8 @@ package trop;
 
 import net.minecraft.src.*;
 
+import java.util.List;
+
 public class TROPItemRingBase extends Item {
 	public TROPItemRingBase(int id) {
 		super(id);
@@ -11,16 +13,21 @@ public class TROPItemRingBase extends Item {
 		onUpdate(itemStack, world, entity, i, b);
 	}
 
+	public void a(ItemStack itemStack, List list) {
+		addInformation(itemStack, list);
+	}
+
 	public ItemStack a(ItemStack itemStack, World world, EntityPlayer entityPlayer) {
 		return onItemRightClick(itemStack, world, entityPlayer);
 	}
 
-	@Override
 	public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer entityPlayer) {
 		return itemStack;
 	}
 
-	@Override
+	public void addInformation(ItemStack itemStack, List list) {
+	}
+
 	public void onUpdate(ItemStack itemStack, World world, Entity entity, int i, boolean b) {
 	}
 }
