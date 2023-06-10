d3451b558e3cdfb0dc67e99ab183484aad54b550
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-06-10 13:25:30 +0300

    Update TROP.java

--- 1.8.9/src/main/java/trop/TROP.java
+++ 1.8.9/src/main/java/trop/TROP.java
@@ -16,6 +16,8 @@ import java.util.Set;
 
 @Mod(modid = "trop")
 public class TROP {
+	public static final String DISABLE_CURSEFORGE_DUPLICATE_NOTICE = "130110062023";
+	
 	public static final Set<Item> CONTENT = new HashSet<>();
 	@SidedProxy(serverSide = "trop.TROPCommonProxy", clientSide = "trop.TROPClientProxy")
 	public static TROPCommonProxy proxy;
