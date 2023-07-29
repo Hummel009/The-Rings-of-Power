c1af32aec69e495e6b2eac2ecb76ba85bd30b4a4
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-07-29 15:24:50 +0300

    to ignore fully

--- 1.13.2/src/main/java/trop/TROPItemRingBase.java
+++
@@ -1,9 +0,0 @@
-package trop;
-
-import net.minecraft.item.Item;
-
-public class TROPItemRingBase extends Item {
-	public TROPItemRingBase() {
-		super(new Properties().defaultMaxDamage(0).group(TROPCreativeTabs.TAB_RINGS));
-	}
-}
--- 1.16.5/src/main/java/trop/TROPItemRingBase.java
+++
@@ -1,9 +0,0 @@
-package trop;
-
-import net.minecraft.item.Item;
-
-public class TROPItemRingBase extends Item {
-	public TROPItemRingBase() {
-		super(new Properties().durability(0).tab(TROPCreativeTabs.TAB_RINGS));
-	}
-}
