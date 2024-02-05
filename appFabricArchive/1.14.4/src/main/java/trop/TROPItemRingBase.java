751d0931e917a86ee655e82a196c8b7918691664
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-05 22:55:35 +0300

    fabric ready

--- appFabricArchive/1.14.4/src/main/java/trop/TROPItemRingBase.java
+++ appFabricArchive/1.14.4/src/main/java/trop/TROPItemRingBase.java
@@ -4,7 +4,7 @@ import net.minecraft.world.item.Item;
 
 public class TROPItemRingBase extends Item {
 	public TROPItemRingBase() {
-		super(new Properties().durability(0).tab(TROP.TAB_RINGS));
+		super(new Properties().durability(0));
 		TROP.CONTENT.add(this);
 	}
 }
