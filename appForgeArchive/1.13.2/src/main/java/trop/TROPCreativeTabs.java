388d44f87e3bc42f7e495720159b895a24075fba
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-06 13:35:34 +0300

    13

--- appForgeArchive/1.13.2/src/main/java/trop/TROPCreativeTabs.java
+++ appForgeArchive/1.13.2/src/main/java/trop/TROPCreativeTabs.java
@@ -4,7 +4,7 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 
 public class TROPCreativeTabs {
-	public static final ItemGroup TAB_RINGS = new ItemGroup("ringPower") {
+	public static final ItemGroup TAB_RINGS = new ItemGroup("trop.rings") {
 		@Override
 		public ItemStack createIcon() {
 			return new ItemStack(TROP.ringNarya);
