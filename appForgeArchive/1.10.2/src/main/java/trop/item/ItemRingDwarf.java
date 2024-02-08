d590646c1afcb12bfb4320dc0c48f873a73593d1
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-08 15:29:58 +0300

    1

--- appForgeArchive/1.10.2/src/main/java/trop/item/ItemRingDwarf.java
+++ appForgeArchive/1.10.2/src/main/java/trop/item/ItemRingDwarf.java
@@ -32,7 +32,7 @@ public class ItemRingDwarf extends ItemRingBase {
 	}
 
 	@Override
-	public void onUpdate(ItemStack itemStack, World world, Entity entity, int i, boolean b) {
+	public void onUpdate(ItemStack itemStack, World world, Entity entity, int slot, boolean selected) {
 		if (entity instanceof EntityLivingBase) {
 			((EntityLivingBase) entity).addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 20, 1));
 			((EntityLivingBase) entity).addPotionEffect(new PotionEffect(MobEffects.MINING_FATIGUE, 20, 1));
