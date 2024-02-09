910ea230e7635e0def69c0b211546a59a703bdc3
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-09 14:48:10 +0300

    1

--- appFabricArchive/1.15.2/src/main/java/com/github/hummel/trop/item/ItemRingVilya.java
+++ appFabricArchive/1.15.2/src/main/java/com/github/hummel/trop/item/ItemRingVilya.java
@@ -26,7 +26,7 @@ public class ItemRingVilya extends ItemRing {
 	}
 
 	@Override
-	public void inventoryTick(ItemStack itemStack, Level level, Entity entity, int i, boolean b) {
+	public void inventoryTick(ItemStack itemStack, Level level, Entity entity, int slot, boolean advanced) {
 		if (entity instanceof LivingEntity) {
 			((LivingEntity) entity).addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 20, 1));
 			((LivingEntity) entity).addEffect(new MobEffectInstance(MobEffects.JUMP, 20, 1));
