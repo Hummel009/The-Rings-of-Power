a29027f78f2e8b1a3ea535b5077f96d80ceef333
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-09 13:22:28 +0300

    2

--- appForgeArchive/1.11.2/src/main/java/trop/Main.java
+++ appForgeArchive/1.11.2/src/main/java/trop/Main.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLMissingMappingsEvent;
 import net.minecraftforge.fml.common.eventhandler.EventBus;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import trop.handler.EventHandler;
+import trop.handler.ForgeEventHandler;
 import trop.init.Items;
 
 import java.util.HashMap;
@@ -17,9 +17,9 @@ public class Main {
 	public static final String DISABLE_CURSEFORGE_DUPLICATE_NOTICE = "131829122023";
 
 	public Main() {
-		EventBus eventBus = MinecraftForge.EVENT_BUS;
-		EventHandler eventHandler = new EventHandler();
-		eventBus.register(eventHandler);
+		EventBus forgeEventBus = MinecraftForge.EVENT_BUS;
+		ForgeEventHandler forgeEventHandler = new ForgeEventHandler();
+		forgeEventBus.register(forgeEventHandler);
 	}
 
 	@Mod.EventHandler
--- appForgeArchive/1.11.2/src/main/java/trop/handler/ForgeEventHandler.java
+++ appForgeArchive/1.11.2/src/main/java/trop/handler/ForgeEventHandler.java
@@ -10,7 +10,7 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import trop.init.Items;
 
-public class EventHandler {
+public class ForgeEventHandler {
 	@SubscribeEvent
 	public void onItemRegistry(RegistryEvent.Register<Item> event) {
 		Items.register();
