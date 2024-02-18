7347a6884cbfe39753fea3761303ffed7483df5b
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-18 18:16:33 +0300

    11

--- appForgeArchive/1.11.2/src/main/java/com/github/hummel/trop/item/ItemRingNenya.java
+++ appForgeArchive/1.11.2/src/main/java/com/github/hummel/trop/item/ItemRingNenya.java
@@ -1,40 +1,16 @@
 package com.github.hummel.trop.item;
 
-import com.mojang.realmsclient.gui.ChatFormatting;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
-import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.text.translation.I18n;
-import net.minecraft.world.World;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemRingNenya extends ItemRing {
 	@Override
-	@SuppressWarnings("deprecation")
-	public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer, List<String> list, boolean advanced) {
-		for (Potion potion : new Potion[]{MobEffects.WATER_BREATHING}) {
-			list.add(ChatFormatting.DARK_GREEN + I18n.translateToLocal(potion.getName()).trim());
-		}
-	}
-
-	@Override
-	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer entityPlayer, EnumHand hand) {
-		entityPlayer.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 3600, 2));
-		entityPlayer.addPotionEffect(new PotionEffect(MobEffects.HEALTH_BOOST, 3600, 2));
-		return super.onItemRightClick(world, entityPlayer, hand);
-	}
-
-	@Override
-	public void onUpdate(ItemStack itemStack, World world, Entity entity, int slot, boolean selected) {
-		if (entity instanceof EntityLivingBase) {
-			((EntityLivingBase) entity).addPotionEffect(new PotionEffect(MobEffects.WATER_BREATHING, 20));
-		}
+	public List<PotionEffect> getPotionEffects() {
+		List<PotionEffect> potionEffects = new ArrayList<>();
+		potionEffects.add(new PotionEffect(MobEffects.WATER_BREATHING, 20));
+		return potionEffects;
 	}
 }
--- appForgeArchive/1.11.2/src/main/java/com/github/hummel/trop/item/ItemRingVilya.java
+++ appForgeArchive/1.11.2/src/main/java/com/github/hummel/trop/item/ItemRingVilya.java
@@ -1,41 +1,17 @@
 package com.github.hummel.trop.item;
 
-import com.mojang.realmsclient.gui.ChatFormatting;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
-import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.text.translation.I18n;
-import net.minecraft.world.World;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemRingVilya extends ItemRing {
 	@Override
-	@SuppressWarnings("deprecation")
-	public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer, List<String> list, boolean advanced) {
-		for (Potion potion : new Potion[]{MobEffects.SPEED, MobEffects.JUMP_BOOST}) {
-			list.add(ChatFormatting.DARK_GREEN + I18n.translateToLocal(potion.getName()).trim());
-		}
-	}
-
-	@Override
-	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer entityPlayer, EnumHand hand) {
-		entityPlayer.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 3600, 2));
-		entityPlayer.addPotionEffect(new PotionEffect(MobEffects.HEALTH_BOOST, 3600, 2));
-		return super.onItemRightClick(world, entityPlayer, hand);
-	}
-
-	@Override
-	public void onUpdate(ItemStack itemStack, World world, Entity entity, int slot, boolean selected) {
-		if (entity instanceof EntityLivingBase) {
-			((EntityLivingBase) entity).addPotionEffect(new PotionEffect(MobEffects.SPEED, 20, 1));
-			((EntityLivingBase) entity).addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 20, 1));
-		}
+	public List<PotionEffect> getPotionEffects() {
+		List<PotionEffect> potionEffects = new ArrayList<>();
+		potionEffects.add(new PotionEffect(MobEffects.SPEED, 20, 1));
+		potionEffects.add(new PotionEffect(MobEffects.JUMP_BOOST, 20, 1));
+		return potionEffects;
 	}
 }
