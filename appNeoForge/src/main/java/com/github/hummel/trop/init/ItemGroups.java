e500d286280a2b9a2a3a3b3945a8fb82fd906bb1
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-19 23:40:32 +0300

    REGISTRY

--- appNeoForge/src/main/java/com/github/hummel/trop/init/ItemGroups.java
+++ appNeoForge/src/main/java/com/github/hummel/trop/init/ItemGroups.java
@@ -10,9 +10,9 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 
 @SuppressWarnings("WeakerAccess")
 public class ItemGroups {
-	public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(BuiltInRegistries.CREATIVE_MODE_TAB, "trop");
+	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(BuiltInRegistries.CREATIVE_MODE_TAB, "trop");
 
-	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> TAB_RINGS = CREATIVE_TABS.register("rings", () -> CreativeModeTab.builder().title(Component.translatable("itemGroup.trop.rings")).icon(() -> new ItemStack(Items.RING_NARYA.get())).displayItems((enabledFlags, populator) -> {
+	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> TAB_RINGS = REGISTRY.register("rings", () -> CreativeModeTab.builder().title(Component.translatable("itemGroup.trop.rings")).icon(() -> new ItemStack(Items.RING_NARYA.get())).displayItems((enabledFlags, populator) -> {
 		for (var item : Items.REGISTRY.getEntries()) {
 			populator.accept(item.get());
 		}
@@ -22,6 +22,6 @@ public class ItemGroups {
 	}
 
 	public static void register(IEventBus modEventBus) {
-		CREATIVE_TABS.register(modEventBus);
+		REGISTRY.register(modEventBus);
 	}
 }
