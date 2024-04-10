3acd20ab4ed44612b833074ff326a839aae1cf9b
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-04-10 19:24:03 +0300

    retrofuturagradle

--- appForgeArchive/build.gradle.kts
+++ appForgeArchive/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
 	id("net.minecraftforge.gradle") version "[6.0.16,6.2)" apply false
-	id("com.gtnewhorizons.retrofuturagradle") version "1.3.30" apply false
+	id("com.gtnewhorizons.retrofuturagradle") version "1.3.35" apply false
 	id("agency.highlysuspect.voldeloom") version "2.5-SNAPSHOT" apply false
 }
