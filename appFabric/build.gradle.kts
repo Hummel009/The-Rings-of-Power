0a0ed1a56ba64d2b524d55867d5173b6a8c14f65
Hummel009 <hummel.turbamentum@gmail.com>
2024-05-13 11:17:26 +0300

    Update build.gradle.kts

--- appFabric/build.gradle.kts
+++ appFabric/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
 	minecraft("com.mojang:minecraft:1.20.6")
 	mappings(loom.officialMojangMappings())
 	modImplementation("net.fabricmc:fabric-loader:0.15.11")
-	modImplementation("net.fabricmc.fabric-api:fabric-api:0.97.8+1.20.6")
+	modImplementation("net.fabricmc.fabric-api:fabric-api:0.98.0+1.20.6")
 }
 
 java {
