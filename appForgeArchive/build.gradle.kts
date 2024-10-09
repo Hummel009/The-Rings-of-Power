a8571956f2c35ba14c4dba512f1a4824ff61bcb5
Hummel009 <hummel.turbamentum@gmail.com>
2024-10-09 20:41:11 +0300

    Delete build.gradle.kts

--- appForgeArchive/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins {
-	id("net.minecraftforge.gradle") version "latest.release" apply false
-	id("com.gtnewhorizons.retrofuturagradle") version "latest.release" apply false
-	id("agency.highlysuspect.voldeloom") version "latest.release" apply false
-}
